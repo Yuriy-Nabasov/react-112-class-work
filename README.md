4781c28526c44e51f0a34aec328e893cb54af3a7
Yuriy Nabasov <nabasoff@gmail.com>
2025-03-07 19:05:27 +0200

    leson-06

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
-# Заняття 5 - [Форми в React](https://youtu.be/qkQAGtydAHs)
+# [Заняття 6 - Форми з Formik](https://youtu.be/RbGgd3nUWxs)
 
-- [Форми (не контрольовані - отримуємо значення тільки під час Submit)](https://youtu.be/qkQAGtydAHs?t=45)
-  - Форма як окремий компонент (UserForm)
-  - Сабміт форми (onSubmit)
-  - Неконтрольована форма з e.target.elements
-  - Очищення форми після сабміту з e.target.reset()
-  - Пропс сабміту
-  - Об'єкт подіі не передаємо у пропс сабміту (event не залишає форму)
-- [Хук useId](https://youtu.be/qkQAGtydAHs?t=1529)
-- [Контрольовані елементи](https://youtu.be/qkQAGtydAHs?t=2014)
-  - Атрибути value та onChange та передача їх значень пропсами
-  - [Текстове поле (TextInput)](https://youtu.be/qkQAGtydAHs?t=2473)
-  - [Елемент select (LangSwitcher)](https://youtu.be/qkQAGtydAHs?t=2981)
-- [Контрольована форма (OrderForm)](https://youtu.be/qkQAGtydAHs?t=3839)
-  - Обробка сабміту форми
-  - Очищення форми після сабміту
-- [Прокидання пропсів](https://youtu.be/qkQAGtydAHs?t=4844)
-- [Kahoot](https://youtu.be/qkQAGtydAHs?t=5569)
-- [Відповіді на питання](https://youtu.be/qkQAGtydAHs?t=6154)
+- Бібліотека [Formik](https://formik.org/)
+- Контейнер форми Formik та Form
+  - [Пропс initialValues](https://youtu.be/RbGgd3nUWxs?t=153)
+  - Пропс onSubmit
+- Поля форми Field
+  - Атрибут name
+  - Початкове значення елементів
+- Відправка форми
+  - Параметри values та actions у onSubmit
+- Стилізація компонентів Formik
+- Типи полів через пропс as
+- [Валідація](https://youtu.be/RbGgd3nUWxs?t=2062) з [Yup](https://github.com/jquense/yup)
+  - Схема валідаціі
+  - Можливі аргументи функції-валідатора
+  - Пропс validationSchema
+- [Компонент ErrorMessage](https://youtu.be/RbGgd3nUWxs?t=3753)
+
+  - [Пропс component](https://youtu.be/RbGgd3nUWxs?t=4087)
+
+- [Kahoot](https://youtu.be/RbGgd3nUWxs?t=4742)
+- [Відповіді на питання](https://youtu.be/RbGgd3nUWxs?t=5333)
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,86 +1,15 @@
-// import AppBar from '../AppBar/AppBar';
-// import OrderForm from '../OrderForm/OrderForm';
-import { useState } from 'react';
 import UserForm from '../UserForm/UserForm';
 import css from './App.module.css';
-import TextInput from '../TextInput/TextInput';
-import LangSwitcher from '../LangSwitcher/LangSwitcher';
-import OrderForm from '../OrderForm/OrderForm';
-import AppBar from '../AppBar/AppBar';
 
-//? Не контрольовані форми
-// export default function App() {
-//   const [users, setUsers] = useState([]);
-//   const addUser = (newUser) => {
-//     setUsers((prevUsers) => {
-//       return [...prevUsers, newUser];
-//     });
-//   };
-//   return (
-//     <div className={css.container}>
-//       <h1>Forms in React</h1>
-//       <UserForm onAdd={addUser} />
-//     </div>
-//   );
-// }
-
-// ? Контрольовані елементи
-// export default function App() {
-//   const [inputValue, setInputValue] = useState('');
-//   const handleInputChange = (newValue) => {
-//     // console.log(event);
-//     setInputValue(newValue);
-//   };
-//   const [lang, setLang] = useState(`en`);
-//   const handleChangeLang = (newLang) => {
-//     console.log(newLang);
-//     setLang(newLang);
-//   };
-//   return (
-//     <div className={css.container}>
-//       <h1>Forms in React</h1>
-//       <LangSwitcher value={lang} onSelect={handleChangeLang} />
-//       <h2>Current lng: {lang}</h2>
-//       <TextInput value={inputValue} onFilter={handleInputChange} />
-//       {/* <input type="" value={inputValue} onChange={handleChange} /> */}
-//       <h2>Entered text: {inputValue}</h2>
-//     </div>
-//   );
-// }
-
-// ? Контрольована форма
-// export default function App() {
-
-//   return (
-//     <div className={css.container}>
-//       <h1>Forms in React</h1>
-//       <OrderForm onOrder={(newOrder) => console.log(newOrder)} />
-//     </div>
-//   );
-// }
-
-// ? Прокидання пропсів
 export default function App() {
-  const [filter, setFilter] = useState('');
-  const [lang, setLang] = useState(`en`);
-  const handleFilterChange = (newValue) => {
-    setFilter(newValue);
+  const addUser = (newUser) => {
+    console.log(`Adding new user:`, newUser);
   };
-  const handleChangeLang = (newLang) => {
-    setLang(newLang);
-  };
+
   return (
     <div className={css.container}>
-      {/* <h1>Forms in React</h1> */}
-      <AppBar
-        filter={filter}
-        lang={lang}
-        onFilter={handleFilterChange}
-        onSelectLang={handleChangeLang}
-      />
-      <h2>Current lng: {lang}</h2>
-      <h2>Filter: {filter}</h2>
-      <OrderForm onOrder={(newOrder) => console.log(newOrder)} />
+      <h1>Forms with Formik</h1>
+      <UserForm onAdd={addUser} />
     </div>
   );
 }
