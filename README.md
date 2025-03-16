673a0dbf071a6d009d72d3d780e80c811d956c82
Yuriy Nabasov <nabasoff@gmail.com>
2025-03-16 11:03:09 +0200

    lesson-07

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
-# [Заняття 6 - Форми з Formik](https://youtu.be/RbGgd3nUWxs)
+# [Заняття 7 - HTTP-запити](https://youtu.be/kvuXrC-ajn4)
 
-- Бібліотека [Formik](https://formik.org/)
-- Контейнер форми Formik та Form
-  - [Пропс initialValues](https://youtu.be/RbGgd3nUWxs?t=153)
-  - Пропс onSubmit
-- Поля форми Field
-  - Атрибут name
-  - Початкове значення елементів
-- Відправка форми
-  - Параметри values та actions у onSubmit
-- Стилізація компонентів Formik
-- Типи полів через пропс as
-- [Валідація](https://youtu.be/RbGgd3nUWxs?t=2062) з [Yup](https://github.com/jquense/yup)
-  - Схема валідаціі
-  - Можливі аргументи функції-валідатора
-  - Пропс validationSchema
-- [Компонент ErrorMessage](https://youtu.be/RbGgd3nUWxs?t=3753)
+- HTTP-запити
+  - https://hn.algolia.com/api (http://hn.algolia.com/api/v1/search)
+  - [Запит в ефекті при монтуванні або оновлені компонента](https://youtu.be/kvuXrC-ajn4?t=253)
+  - [Запит при події, наприклад сабміт](https://youtu.be/kvuXrC-ajn4?t=603)
+  - Оголошення функціі у ефекті
+  - Обробка даних запиту
+  - Відображення даних
+  - Поділ відповідальності
+  - [Індикатор завантаження](https://youtu.be/kvuXrC-ajn4?t=1609)
+  - [Обробка помилок](https://youtu.be/kvuXrC-ajn4?t=1994)
+  - [Пошук через форму](https://youtu.be/kvuXrC-ajn4?t=2519)
+- [Пагінація](https://youtu.be/kvuXrC-ajn4?t=2691)
+  - Параметри групи та номеру групи
+  - Кнопка Load more
+  - Зберігання `page` та `topic` у стані
+- [Бібліотека](https://youtu.be/kvuXrC-ajn4?t=4588) [React Hot Toast](https://react-hot-toast.com/)
 
-  - [Пропс component](https://youtu.be/RbGgd3nUWxs?t=4087)
-
-- [Kahoot](https://youtu.be/RbGgd3nUWxs?t=4742)
-- [Відповіді на питання](https://youtu.be/RbGgd3nUWxs?t=5333)
+- [Kahoot](https://youtu.be/kvuXrC-ajn4?t=4969)
+- [Відповіді на питання](https://youtu.be/kvuXrC-ajn4?t=5603)
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,134 @@
-import UserForm from '../UserForm/UserForm';
+import { useEffect, useState } from 'react';
+import { fetchArticles } from '../../articleService';
 import css from './App.module.css';
+import SearchForm from '../SearchForm/SearchForm';
+import ArticleList from '../ArticleList/ArticleList';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function App() {
-  const addUser = (newUser) => {
-    console.log(`Adding new user:`, newUser);
-  };
+    // const [clicks, setClicks] = useState(0);
 
-  return (
-    <div className={css.container}>
-      <h1>Forms with Formik</h1>
-      <UserForm onAdd={addUser} />
-    </div>
-  );
+    // useEffect(() => {
+    //     fetchArticles()
+    //         .then((data) => console.log(data))
+    //         .catch();
+    // }, []);
+
+    // useEffect(() => {
+    //     console.log(clicks);
+    //     async function getArticles() {
+    //         try {
+    //             const data = await fetchArticles();
+    //             console.log(data);
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     getArticles();
+    // }, [clicks]);
+
+    // ? Pattern HTTP-запиту
+    //! 1. fetch data
+    //! 2. save to state
+    //! 3. state update
+    //! 4. component update
+
+    const [articles, setArticles] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [error, setError] = useState(false);
+
+    // ? Pattern pagination
+    //! 1. Form submit
+    //! 2. http when?
+    // !   1) form submission (topic change)
+    // !   2) change page
+    //! 3. state update
+    //! 4. component update
+
+    const [page, setPage] = useState(1);
+    const [searchTerm, setSearchTerm] = useState(``);
+
+    // const handleSearch = async (topic) => {
+    //     try {
+    //         setError(false);
+    //         setIsLoading(true);
+    //         setArticles([]);
+    //         const data = await fetchArticles(topic);
+    //         // console.log(data);
+    //         setArticles(data);
+    //         // setIsLoading(false);
+    //     } catch (error) {
+    //         // console.log(error);
+    //         setError(true);
+    //     } finally {
+    //         setIsLoading(false);
+    //     }
+    // };
+
+    const handleSearch = (topic) => {
+        console.log(`${topic}/${Date.now()}`);
+        // setSearchTerm(topic);
+        setSearchTerm(`${topic}/${Date.now()}`);
+        setPage(1);
+        setArticles([]);
+    };
+
+    useEffect(() => {
+        if (searchTerm === ``) {
+            return;
+        }
+        async function getData() {
+            try {
+                setError(false);
+                setIsLoading(true);
+                console.log(searchTerm.split(`/`)[0]);
+                // const data = await fetchArticles(searchTerm, page);
+                const data = await fetchArticles(
+                    searchTerm.split(`/`)[0],
+                    page
+                );
+                setArticles((prevArticles) => {
+                    return [...prevArticles, ...data];
+                });
+            } catch {
+                setError(true);
+                toast.error(`Please reload there was an error!!!`);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        getData();
+        console.log(page, searchTerm);
+    }, [page, searchTerm]);
+
+    return (
+        <div className={css.container}>
+            {/* <h1>HTTP request in React</h1> */}
+            {/* <button onClick={() => setClicks(clicks + 1)}>
+                Click me {clicks}
+            </button> */}
+            <SearchForm onSearch={handleSearch} />
+
+            {error && <b>Whoops there was an error plz reload...</b>}
+
+            <ArticleList items={articles} />
+
+            {isLoading && <b>Loading data, please wait...</b>}
+
+            {/* {articles.length > 0 && !isLoading && (
+                <button onClick={() => setPage(page + 1)}>
+                    Load more articles {page}
+                </button>
+            )} */}
+            {/* або */}
+            {articles.length > 0 && (
+                <button onClick={() => setPage(page + 1)} disabled={isLoading}>
+                    Load more articles {page}
+                </button>
+            )}
+            <Toaster position="top-right" />
+        </div>
+    );
 }
