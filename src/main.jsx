738a464b81e831fa383afa19ed9134ee9e5f3891
Yuriy Nabasov <nabasoff@gmail.com>
2025-03-31 20:10:05 +0300

    lesson-11

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import 'modern-normalize';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import { store } from './redux/store';
 import './index.css';
-import { BrowserRouter } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
