e896b331f3d753dc0e10b3919ce423f5df193d4b
Yuriy Nabasov <nabasoff@gmail.com>
2025-04-11 23:18:13 +0300

    lesson-15

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from './components/App/App';
+import App from './components/App';
 import { store } from './redux/store';
 import 'modern-normalize';
 import './index.css';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    {/* <PersistGate persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
